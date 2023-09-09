e0a9154ba281ce3208316b04c1b688e6abcf4f63
Sy Truong <sy.truong@detectorinspector.com.au>
2023-09-09 11:25:07 +0700

    feat(problem1): add sum_to_n function with n param

---
+++ src/problem1/problem1.js
@@ -0,0 +1,21 @@
+// Using for loop
+var sum_to_n_a = function(n) {
+    var sum = 0;
+    for (var i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+};
+
+// Using Recursion
+var sum_to_n_b = function(n) {
+    if (n === 1) {
+        return 1;
+    }
+    return n + sum_to_n_b(n - 1);
+};
+
+// Using the Arithmetic Progression Formula
+var sum_to_n_c = function(n) {
+    return (n * (n + 1)) / 2;
+};

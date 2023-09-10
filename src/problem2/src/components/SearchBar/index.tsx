d4d4503c40a5eff713bf0fac8eeb0a1af7768ef3
Sy Truong <sy.truong@detectorinspector.com.au>
2023-09-10 12:56:35 +0700

    chore(problem2): add header of the page

---
+++ src/problem2/src/components/SearchBar/index.tsx
@@ -0,0 +1,35 @@
+function SearchBar() {
+  return (
+    <div className="w-full max-w-lg lg:max-w-xs border border-solid border-[#2222220d] rounded-xl">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <div className="relative">
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <svg
+            className="h-5 w-5 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <input
+          id="search"
+          name="search"
+          className="block w-full rounded-xl border border-solid border-transparent bg-white py-2 pl-10 pr-3 leading-5 placeholder-gray-500 focus:border-indigo-500 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+          placeholder="Search tokens and NFT collections"
+          type="search"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
